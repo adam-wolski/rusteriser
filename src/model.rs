bc6273168ac5dcb13ccd9b832f3b05cbab124760
Adam Wolski <miniukof@gmail.com>
2016-04-22 08:15:25 +0200

    Check if mesh provided normals and uvs.

--- src/model.rs
+++ src/model.rs
@@ -3,7 +3,7 @@ use tobj;
 use cgmath::*;
 
 #[derive(Debug, Clone)]
-pub struct Vertex{
+pub struct Vertex {
     pub pos: Vector3<f32>,
     pub normal: Vector3<f32>,
     pub uv: Vector2<f32>,
@@ -34,40 +34,29 @@ impl Model {
         let mut faces: Vec<Face> = Vec::with_capacity(mesh.indices.len() / 3);
         for i in (0..mesh.indices.len()).filter(|i| i % 3 == 0) {
 
-            let mut face = Face {
-                verts: Vec::with_capacity(3),
-            };
+            let mut face = Face { verts: Vec::with_capacity(3) };
             // TODO: Error check for when there are no normals or uvs provided.
-            face.verts.push(Vertex {
-                pos: Vector3::<f32>::new(mesh.positions[mesh.indices[i] as usize * 3],
-                                         mesh.positions[mesh.indices[i] as usize * 3 + 1],
-                                         mesh.positions[mesh.indices[i] as usize * 3 + 2]),
-                normal: Vector3::<f32>::new(mesh.normals[mesh.indices[i] as usize * 3],
-                                            mesh.normals[mesh.indices[i] as usize * 3 + 1],
-                                            mesh.normals[mesh.indices[i] as usize * 3 + 2]),
-                uv: Vector2::<f32>::new(mesh.texcoords[mesh.indices[i] as usize * 2],
-                                        mesh.texcoords[mesh.indices[i] as usize * 2 + 1]),
-            });
-            face.verts.push(Vertex {
-                pos: Vector3::<f32>::new(mesh.positions[mesh.indices[i + 1] as usize * 3],
-                                         mesh.positions[mesh.indices[i + 1] as usize * 3 + 1],
-                                         mesh.positions[mesh.indices[i + 1] as usize * 3 + 2]),
-                normal: Vector3::<f32>::new(mesh.normals[mesh.indices[i + 1] as usize * 3],
-                                            mesh.normals[mesh.indices[i + 1] as usize * 3 + 1],
-                                            mesh.normals[mesh.indices[i + 1] as usize * 3 + 2]),
-                uv: Vector2::<f32>::new(mesh.texcoords[mesh.indices[i + 1] as usize * 2],
-                                        mesh.texcoords[mesh.indices[i + 1] as usize * 2 + 1]),
-            });
-            face.verts.push(Vertex {
-                pos: Vector3::<f32>::new(mesh.positions[mesh.indices[i + 2] as usize * 3],
-                                         mesh.positions[mesh.indices[i + 2] as usize * 3 + 1],
-                                         mesh.positions[mesh.indices[i + 2] as usize * 3 + 2]),
-                normal: Vector3::<f32>::new(mesh.normals[mesh.indices[i + 2] as usize * 3],
-                                            mesh.normals[mesh.indices[i + 2] as usize * 3 + 1],
-                                            mesh.normals[mesh.indices[i + 2] as usize * 3 + 2]),
-                uv: Vector2::<f32>::new(mesh.texcoords[mesh.indices[i + 2] as usize * 2],
-                                        mesh.texcoords[mesh.indices[i + 2] as usize * 2 + 1]),
-            });
+
+            for m in 0..3 {
+                face.verts.push(Vertex {
+                    pos: Vector3::<f32>::new(mesh.positions[mesh.indices[i + m] as usize * 3],
+                                             mesh.positions[mesh.indices[i + m] as usize * 3 + 1],
+                                             mesh.positions[mesh.indices[i + m] as usize * 3 + 2]),
+                    normal: if mesh.normals.is_empty() {
+                        Vector3::<f32>::new(1.0, 1.0, 1.0)
+                    } else {
+                        Vector3::<f32>::new(mesh.normals[mesh.indices[i + m] as usize * 3],
+                                            mesh.normals[mesh.indices[i + m] as usize * 3 + 1],
+                                            mesh.normals[mesh.indices[i + m] as usize * 3 + 2])
+                    },
+                    uv: if mesh.texcoords.is_empty() {
+                        Vector2::<f32>::new(0.0, 0.0)
+                    } else {
+                        Vector2::<f32>::new(mesh.texcoords[mesh.indices[i + m] as usize * 2],
+                                            mesh.texcoords[mesh.indices[i + m] as usize * 2 + 1])
+                    },
+                });
+            }
             faces.push(face);
         }
         faces
