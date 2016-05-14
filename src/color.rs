25b8cefbdd6709d7b8d309a98bdd60ed60ae73a5
Adam Wolski <miniukof@gmail.com>
2016-05-14 21:01:30 +0200

    Change naming of functions to more descriptive.

--- src/color.rs
+++ src/color.rs
@@ -4,7 +4,7 @@ use utils;
 
 
 /// Represent color values as 0.0 - 1.0 ranges. For (u8, u8, u8, u8) tuple.
-pub fn as_ranges(clr: (u8, u8, u8, u8)) -> (f32, f32, f32, f32) {
+pub fn tup8_as_ranges(clr: (u8, u8, u8, u8)) -> (f32, f32, f32, f32) {
     (clr.0 as f32 / 255.0,
      clr.1 as f32 / 255.0,
      clr.2 as f32 / 255.0,
@@ -13,7 +13,7 @@ pub fn as_ranges(clr: (u8, u8, u8, u8)) -> (f32, f32, f32, f32) {
 
 /// Represent color as u8 values.
 /// Made consistent with usage in shaders, where we return Vec4 instead of tuple.
-pub fn as_values(clr: Vector4<f32>) -> (u8, u8, u8, u8) {
+pub fn v4_as_values(clr: Vector4<f32>) -> (u8, u8, u8, u8) {
     ((clr.x * 255.0).round().floor() as u8,
      (clr.y * 255.0).round().floor() as u8,
      (clr.z * 255.0).round().floor() as u8,
@@ -23,7 +23,7 @@ pub fn as_values(clr: Vector4<f32>) -> (u8, u8, u8, u8) {
 /// Represent color as u32 value.
 /// Made consistent with usage in shaders, where we return Vec4 instead of tuple.
 /// Assumes BGRA order of colors.
-pub fn as_value(clr: Vector4<f32>) -> u32 {
+pub fn v4_as_value(clr: Vector4<f32>) -> u32 {
     let clr_u8 = ((clr.w * 255.0).round().floor() as u8,
                   (clr.x * 255.0).round().floor() as u8,
                   (clr.y * 255.0).round().floor() as u8,
